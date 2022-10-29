96e3198c8f93e3de43ce3ea402c44b4604af884b
Ignacio Cunial <igsecu@hotmail.com>
2022-10-29 10:18:27 -0300

    changes

--- index.js
+++ index.js
@@ -4,18 +4,10 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-const server = http.createServer(app);
+const cors = require("cors");
 
+const server = http.createServer(app);
+server.use(cors());
 const io = socketio(server);
 
 app.get("/", (req, res) => {
