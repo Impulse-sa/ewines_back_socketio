2dde931f2b4b5ccb2b3f6c3fe8ffd45077069671
Ignacio Cunial <igsecu@hotmail.com>
2022-10-29 10:21:37 -0300

    changes

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const http = require("http");
 
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 const server = http.createServer(app);
 
 const io = socketio(server);
