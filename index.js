1ee209a1565ec59971235076039d366b228cf927
Impulse-sa <118229300+Impulse-sa@users.noreply.github.com>
2023-01-12 20:58:23 -0300

    localhost

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
